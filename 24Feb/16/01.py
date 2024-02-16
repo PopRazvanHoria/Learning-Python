feff92db85a0b74f0357aec6a96331cc22a19386
Razvan <PopRazvanHoria>
2024-02-17 01:37:25 +0200

    Decorators

---
+++ 24Feb/16/01.py
@@ -0,0 +1,35 @@
+#Decorators
+
+
+# code for testing decorator chaining 
+def decor1(func): 
+	def inner(): 
+		x = func() 
+		return x * x 
+	return inner 
+
+def decor(func): 
+	def inner(): 
+		x = func() 
+		return 2 * x 
+	return inner 
+
+@decor1
+@decor
+def num(): 
+	return 10
+
+@decor
+@decor1
+def num2():
+	return 10
+
+def num3():
+	return 12
+
+print(num()) 
+print(num2())
+
+print(num()) 
+print(num2())
+print(num3())
